908d223a873016a2293a7bb5fdf7306167762f57
gongthub <1056685839@qq.com>
2017-08-25 00:18:08 +0800

    首页开始排版

--- Codes/AuuZu/js/main.js
+++ Codes/AuuZu/js/main.js
@@ -1,10 +1,17 @@
 /**
  * Created by Administrator on 2017/1/5.
  */
+// wow.js
+new WOW().init();
 $(function() {
 	
+
+//轮播 js
+$('#div-index-img-carousel').carousel({
+	interval: false
+})
 })
 //resize
 $(window).resize(function() {
 	
-})s
+});
